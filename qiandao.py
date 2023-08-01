2ae597c5c19095e2d22d2e1a824f6bcbb663dd68
xxx456 <59285901+xxx456@users.noreply.github.com>
2023-08-01 11:11:42 +0800

    Update qiandao.py

--- qiandao.py
+++ qiandao.py
@@ -8,13 +8,13 @@ url1 = "https://api-takumi.mihoyo.com/event/luna/sign"
 headers = {
     "Connection": "keep-alive",
     "Content-Length": "84",
-    "DS": "1690287736,9n7o4i,b544c2ed81d39178badc5288814d4f8c",
+    "DS": "1690859006,jm4628,ca1a4e7bc6d75f66b2ef1f6e1b95ac42",
     "x-rpc-app_version": "2.53.1",
-    "x-rpc-validate": "c4ef0167fcf1bde3644e46f05cf49827",
+    "x-rpc-validate": "c9de6339175023b5ca10484fde52694e",
     "User-Agent": "Mozilla/5.0 (Linux; Android 13; M2102J2SC Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.210 Mobile Safari/537.36 miHoYoBBS/2.52.1",
     "x-rpc-device_id": "97bcc1b8-d952-35cc-a82a-f243117cc03a",
     "Accept": "application/json, text/plain, */*",
-    "x-rpc-seccode": "c4ef0167fcf1bde3644e46f05cf49827%7Cjordan",
+    "x-rpc-seccode": "c9de6339175023b5ca10484fde52694e%7Cjordan",
     "Content-Type": "application/json;charset=UTF-8",
     "x-rpc-client_type": "5",
     "Origin": "https://webstatic.mihoyo.com",
@@ -25,7 +25,7 @@ headers = {
     "Referer": "https://webstatic.mihoyo.com/",
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Cookie": "ltuid=381745225; login_ticket=2qBydKX9G5qchjkFFXlvoZlt4512uL2Wc6ZieDPx; account_id=381745225; ltoken=2lnvzx9kmBUyp1RIcnuOHoxEXcKiQKYopt12PgFl; _MHYUUID=3775c917-cb27-4476-2a02-fc6b7937e0cc; aliyungf_tc=9149d0c348431c7d045b06fa0d927193ddc1f5bb58a45ddfb43f64f30de07825; mi18nLang=zh-cn; DEVICEFP_SEED_ID=aa0f5e5ced3b8f1c; DEVICEFP_SEED_TIME=1686469981733; cookie_token=x5t4Z3E1T6wCgq4Y18Tmhi6IMloH7Mj5bHUoFPoU; DEVICEFP=38d7ef749e255; _ga=GA1.1.1804909849.1686469967; _ga_HB2EBLW929=GS1.1.1690287726.2.0.1690287731.0.0.0; _ga_J1K3VE5FNG=GS1.1.1690287730.1.0.1690287731.0.0.0"
+    "Cookie": "ltuid=381745225; login_ticket=2qBydKX9G5qchjkFFXlvoZlt4512uL2Wc6ZieDPx; account_id=381745225; ltoken=2lnvzx9kmBUyp1RIcnuOHoxEXcKiQKYopt12PgFl; _MHYUUID=3775c917-cb27-4476-2a02-fc6b7937e0cc; aliyungf_tc=9149d0c348431c7d045b06fa0d927193ddc1f5bb58a45ddfb43f64f30de07825; mi18nLang=zh-cn; DEVICEFP_SEED_ID=aa0f5e5ced3b8f1c; DEVICEFP_SEED_TIME=1686469981733; cookie_token=x5t4Z3E1T6wCgq4Y18Tmhi6IMloH7Mj5bHUoFPoU; DEVICEFP=38d7ef749e255; _ga=GA1.1.1804909849.1686469967; _ga_HB2EBLW929=GS1.1.1690287726.2.0.1690287737.0.0.0; _ga_J1K3VE5FNG=GS1.1.1690287730.1.0.1690287737.0.0.0"
 }
 
 data = {"act_id":"e202304121516551","region":"prod_gf_cn","uid":"113150520","lang":"zh-cn"}
